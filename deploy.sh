d12c7a26626b5136dd3aa5e8d0aaaf51bba3f5f1
Lambros Petrou <lambrospetrou@gmail.com>
2016-09-24 15:16:46 +0100

    Added post for aws s3 sync with git. Also put the script in use using deploy.sh.

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,18 @@
-cd _site/; tar -cvzf lpsite.tgz *; scp lpsite.tgz lambros@lp.gs: ; cd .. ;
+#!/bin/bash
+set -ex
+
+FILES=()
+#for i in $( git diff --name-only ); do
+for i in $( git status -s | sed 's/\s*[a-zA-Z?]\+ \(.*\)/\1/' ); do
+    FILES+=( "$i" )
+done
+#echo "${FILES[@]}"
+
+CMDS=()
+for i in "${FILES[@]}"; do
+    CMDS+=("--include=$i""*")
+done
+#echo "${CMDS[@]}"
+
+echo "${CMDS[@]}" | xargs aws s3 sync . s3://www.lambrospetrou.com --dryrun --delete --exclude "*" 
+#for i in "${FILES[@]}"; do echo "--include=$i""*"; done | xargs aws s3 sync . s3://www.lambrospetrou.com --dryrun --delete --exclude "*" 
