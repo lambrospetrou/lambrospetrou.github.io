6cbf236f9b673e4634772ca9fa244f7f0e632731
Lambros Petrou <lambrospetrou@gmail.com>
2017-09-16 18:56:39 +0100

    Modified the deploy scripts

---
+++ build-tool/deploy.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Run this script giving the number of commits to compare against.
+# By default it will compare with the previous commit and upload the diff.
+# For example: ```./deploy.sh 2``` will diff against the HEAD~3 and upload.
+
+set -ex
+
+if [ -n "$1" ]; then
+    COMMITS="$1"
+else
+    COMMITS="1"
+fi
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#REPO="$(pwd)"
+REPO="$(dirname "$SCRIPT_DIR")"
+
+TMP_DIR="/tmp/lp-website-s3"
+rm -rf "$TMP_DIR"
+mkdir -p "$TMP_DIR"
+pushd "$TMP_DIR"
+git clone "$REPO"
+cd $(basename "$REPO")
+
+# make sure we are clean
+git reset --hard HEAD
+
+# build the latest version
+gomicroblog -site ./
+mv _site _site.latest
+
+# build the previous version
+git reset --hard "HEAD~$COMMITS"
+gomicroblog -site ./
+
+pushd _site
+git init . && git add --all . && git commit -m 'previous'
+
+git checkout -b latest
+find ./ -maxdepth 1 -not -path "./.git*" -not -iname "." -exec rm -rf {} \;
+cp -r ../_site.latest/* .
+
+# Run the script from my article https://www.lambrospetrou.com/articles/aws-s3-sync-git-status/
+"$SCRIPT_DIR/"./git-changes-upload-s3.sh
+
+# Exit the _site and then return to current working directory
+popd
+popd
