f4afdb87452b5dbc69d2daa96cfb3340eb080676
Lambros Petrou <lambrospetrou@gmail.com>
2020-04-21 11:34:34 +0100

    Move NextJS code into `src`

--- lambrospetrou.com/src/pages/index.js
+++ lambrospetrou.com/src/pages/index.js
@@ -4,7 +4,7 @@ import Footer from "../components/footer";
 export default function ArticlesIndex({posts}) {
   return (
     <>
-      <ul class="index-posts">
+      <ul className="index-posts">
         {posts.map(p => <PostEntry key={p.slug} post={p}/>)}
       </ul>
       <Footer />
@@ -17,7 +17,7 @@ function PostEntry({post}) {
     <li>
       <article>
         <header>
-          <h2 class="post-title"><a href={`/articles/${post.slug}`} title="view post">{post.title}</a></h2>
+          <h2 className="post-title"><a href={`/articles/${post.slug}`} title="view post">{post.title}</a></h2>
           {/* <div class="post-meta">{{ $element.FormattedCreatedTime }}</div> */}
         </header>
       </article>
