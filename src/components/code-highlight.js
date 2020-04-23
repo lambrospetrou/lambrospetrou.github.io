29bf9f32226d06581524d187c5237323775d4825
Lambros Petrou <lambrospetrou@gmail.com>
2020-04-23 10:15:44 +0100

    Load `PrismJS` from local scripts and only use CDN for the language

--- src/components/code-highlight.js
+++ src/components/code-highlight.js
@@ -1,12 +1,14 @@
 export const CodeHighlightScripts = ({}) => {
   // I prefer using the following scripts instead of including them in this bundle
   // to avoid delaying them, since the main bundle takes more time to fetch!
-  // Also, since we use the `autoloader` we cannot host them since it uses relative
-  // URLs to load the detected languages, otherwise I would need to get all of the languages I need.
+  // Also, since we use the `autoloader` we provide the `data-autoloader-path`
+  // to avoid having all the languages bundled in this package.
   return (
     <>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.20.0/components/prism-core.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.20.0/plugins/autoloader/prism-autoloader.min.js"></script>
+      {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.20.0/components/prism-core.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.20.0/plugins/autoloader/prism-autoloader.min.js"></script> */}
+      <script src="/s/prismjs/prism-core.min.js"></script>
+      <script src="/s/prismjs/prism-autoloader.min.js" data-autoloader-path="https://cdnjs.cloudflare.com/ajax/libs/prism/1.20.0/components/"></script>
     </>
   );
 };
