987297ce103005021a4f9bf09adcc96f9ebb53fe
Lambros Petrou <lambrospetrou@gmail.com>
2020-04-23 10:37:14 +0100

    Small refactoring of prismjs scripts

--- src/components/code-highlight.js
+++ src/components/code-highlight.js
@@ -5,10 +5,10 @@ export const CodeHighlightScripts = ({}) => {
   // to avoid having all the languages bundled in this package.
   return (
     <>
-      {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.20.0/components/prism-core.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.20.0/plugins/autoloader/prism-autoloader.min.js"></script> */}
-      <script src="/s/prismjs/prism-core.min.js"></script>
-      <script src="/s/prismjs/prism-autoloader.min.js" data-autoloader-path="https://cdnjs.cloudflare.com/ajax/libs/prism/1.20.0/components/"></script>
+      {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.20.0/components/prism-core.min.js"/>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.20.0/plugins/autoloader/prism-autoloader.min.js"/> */}
+      <script src="/s/prismjs/prism-core.min.js"/>
+      <script src="/s/prismjs/prism-autoloader.min.js" data-autoloader-path="https://cdnjs.cloudflare.com/ajax/libs/prism/1.20.0/components/"/>
     </>
   );
 };
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 // We cannot use the CDN URL for the CSS like we do for the JS script because we want
 // to override a few of the CSS properties in our CSS...
-import "../../public/s/prismjs/prism.css";
+import "../../public/s/prismjs/prism.min.css";
 import "../scss/style.scss";
 
 // This default export is required in a new `pages/_app.js` file.
