d919a12e5c7aade4e3d6467ce9e0355bbe1394eb
Lambros Petrou <lambrospetrou@gmail.com>
2021-09-28 20:43:26 +0100

    Add new file for quotes - not used yet anywhere

---
+++ src/lib/data-for-quotes.js
@@ -0,0 +1,20 @@
+// Put new content to the top of the array!
+// interface Entry {
+//   text: string,
+//   author: string,
+//   dateListed: ISO8601 (used for the RSS)
+//   url?: string,
+//   tags?: string[]
+// };
+const QuoteList = [
+    {
+      text: "The most amazing achievement of the computer software industry is its continuing cancellation of the steady and staggering gains made by the computer hardware industry.",
+      author: "Henry Peteroski",
+      dateListed: "2021-09-28T20:42:00.000Z",
+      url: "https://mechanical-sympathy.blogspot.com/2011/07/why-mechanical-sympathy.html",
+    }
+];
+
+module.exports = {
+  QueoteList
+};
