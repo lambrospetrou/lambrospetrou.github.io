803542930e97259390c549ce7693f0f4d6513333
Lambros Petrou <lambrospetrou@gmail.com>
2023-03-05 01:35:48 +0000

    change the header style

--- src/components/layout.js
+++ src/components/layout.js
@@ -72,7 +72,7 @@ const SocialIcon = ({name}) => {
   // Credit to: https://simpleicons.org/?q=github
   switch(name) {
   case "linkedin":
-    return <a href="https://www.linkedin.com/in/lambrospetrou/" title="Lambros Petrou on LinkedIn " target="_blank" rel="noopener"><svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>LinkedIn</title><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/></svg></a>;
+    return <a href="https://www.linkedin.com/in/lambrospetrou/" title="Lambros Petrou on LinkedIn " target="_blank" rel="noopener"><svg role="img" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><title>LinkedIn</title><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/></svg></a>;
   case "twitter":
     return <a href="https://twitter.com/lambrospetrou" title="Tweets" target="_blank" rel="noopener"><svg role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Twitter</title><path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"/></svg></a>;
   case "github":
@@ -88,17 +88,23 @@ const Header = ({}) => {
   return (
     <header id="header-wrapper" className="outer-section">
       <div className="header-signup-strip">
-        <small><a href="/newsletter">Subscribe to my newsletter</a></small>
+        <div className="inner-section">
+          <div style={{display: "flex", gap: "10px", alignItems: "baseline", flexWrap: "wrap", justifyContent: "center"}}>
+            <div id="header-name">&bull;&nbsp;&nbsp;<span style={{color: "#920000", fontWeight: "normal"}}>L</span>ambros <span style={{color: "#920000", fontWeight: "normal"}}>P</span>etrou&nbsp;&nbsp;&bull;</div>
+            <div id="header-quote">"We are what we repeatedly do. Excellence then, is not an act, but a habit!"&#x2009;&mdash;&#x2009;Aristotle</div> 
+          </div>
+          <div style={{display: "flex", gap: "10px", alignItems: "center", flexWrap: "wrap", justifyContent: "center"}}>
+            <div><small><a href="/newsletter">Subscribe to my newsletter</a></small></div> &#x7C;
+            <div id="header-social">
+              <SocialIcon name="github"/>
+              <SocialIcon name="linkedin"/>
+              <SocialIcon name="twitter"/>
+              <SocialIcon name="mastodon"/>
+            </div>
+          </div>
+        </div>
       </div>
       <div id="inner-header" className="inner-section">
-        <div id="header-social">
-          <SocialIcon name="github"/>
-          <SocialIcon name="linkedin"/>
-          <SocialIcon name="twitter"/>
-          <SocialIcon name="mastodon"/>
-        </div>
-        <div id="header-name">Lambros Petrou</div>
-        <div id="header-quote">"We are what we repeatedly do. Excellence then, is not an act, but a habit!"&#x2009;&mdash;&#x2009;Aristotle</div>
         <div id="nav-buttons">
           <a href="/" title="All Articles - Lambros Petrou">Articles</a>
           <a href="/read-watch-listen/" title="My Read-Watch-Listen list">Read-Watch-Listen</a>
