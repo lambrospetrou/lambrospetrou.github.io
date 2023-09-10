542e8fa3eccd6d67ffa1e83b20d68f23cdc3ceac
Lambros Petrou <lambrospetrou@gmail.com>
2023-09-10 17:08:31 +0300

    rwl

--- src/lib/data-for-rwl.js
+++ src/lib/data-for-rwl.js
@@ -14,6 +14,7 @@ const DataList = [
     author: "Gary Vaynerchuk, Jason Fried",
     dateListed: "2023-09-09T10:30:00.000Z",
     type: "audio",
+    tags: ["favourite"],
   },
   {
     url: "https://antonz.org/mastering-curl/",
