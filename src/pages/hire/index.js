327c7afea386bde5ca6ae3b7697d777d5539606b
Lambros Petrou <lambrospetrou@gmail.com>
2023-09-18 16:23:30 +0000

    text edits

--- src/pages/hire/index.js
+++ src/pages/hire/index.js
@@ -4,12 +4,21 @@ import { Layout } from "../../components/layout";
 function HireMeActions() {
   return <div className="cta-container">
     <button className="cta-consult">
-      Book consultation session
+      &#10144; Book consultation session
       <small>1:1 session with me, any topic you want.</small>
     </button>
-    <button className="cta-project">
+    {/* <button className="cta-project">
       Request project contract
       <small>Fixed-duration contracting opportunities.</small>
+    </button> */}
+  </div>
+}
+
+function InterviewConsultingAction() {
+  return <div className="cta-container">
+    <button className="cta-interview">
+      &#10144; Book your interview preparation session
+      <small>1:1 session with me, to prepare you for coding or system design interviews.</small>
     </button>
   </div>
 }
@@ -48,10 +57,12 @@ export default function HireIndex() {
             <div className="intro-text">
               <p>Hi there 👋 I am Lambros.</p>
               <p>
-                I build websites & reliable APIs, implement continuous integration and continuous delivery (CI/CD) pipelines for cloud-native applications, and I can help you scale to millions of users.
+                I build websites & reliable APIs, implement continuous integration and continuous delivery (CI/CD) pipelines for cloud-native applications, and I can help you scale to millions of users.{" "}
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" d="M15.59 14.37a6 6 0 01-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 006.16-12.12A14.98 14.98 0 009.631 8.41m5.96 5.96a14.926 14.926 0 01-5.841 2.58m-.119-8.54a6 6 0 00-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 00-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 01-2.448-2.448 14.9 14.9 0 01.06-.312m-2.24 2.39a4.493 4.493 0 00-1.757 4.306 4.493 4.493 0 004.306-1.758M16.5 9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z" /></svg>
               </p>
               <p>
-                If you want to get a software job in a Big Tech (FAANG) company, I can help you prepare for that too!
+                If you want to get a software job in a Big Tech (FAANG) company, I can help you prepare for that too!{" "}
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5" /></svg>
               </p>
               <p>
                 <strong>Are you already interested?</strong>
@@ -81,11 +92,12 @@ export default function HireIndex() {
         </section>
 
         <section id="cicd">
-          <h3>CI/CD</h3>
+          <h3>CI/CD and Cloud Infrastructure</h3>
           <ul>
             <li>Setup your CI/CD pipeline to automatically test and build your applications on every pull-request.</li>
             <li>Automatic continuous deployment of your software to customers using feature flags, controlled gradual rollouts, and optional automatic rollbacks.</li>
             <li>Infrastructure-as-Code for provision cloud resources (AWS CDK, AWS SAM, Terraform).</li>
+            <li>Integrate with observability tools.</li>
           </ul>
         </section>
 
@@ -98,7 +110,10 @@ export default function HireIndex() {
             <li>Advise and guide you through the interview process for tech companies.</li>
             <li>Do 1:1 mock interview sessions for coding and system design interviews.</li>
           </ul>
+          <p>Read <a href="/articles/big-tech-software-interviews/?ref=lp-hire" title="Article for How to pass the interview for software engineering roles in Big Tech">How to pass the interview for software engineering roles in Big Tech</a> for what to expect during your interview.</p>
         </section>
+
+        <InterviewConsultingAction />
 
         <section id="tech">
           <h3>★ Technology stack ★</h3>
@@ -116,10 +131,8 @@ export default function HireIndex() {
           <p>Favourite stack: Go or Kotlin, AWS Lambda, Cloudflare Pages</p>
         </section>
 
-        <HireMeActions />
-
         <section id="why-trust">
-          <h3>Why you should trust me... And hire me!</h3>
+          <h3>Why you should trust me... And work with me.</h3>
 
           <p>
             I have been creating websites for more than 10 years, even before being paid for it.
@@ -129,7 +142,10 @@ export default function HireIndex() {
             I led the development of the Amazon LOVEFiLM mobile app using the hybrid cross-platform Ionic framework and AngularJS.
           </p>
           <p>I extensively used Java & Kotlin (Spring, Http4k) to server-render websites and their corresponding REST APIs for the Amazon retail website.</p>
+          
+          <blockquote>
           <p>I have deep knowledge of the web platform, and I can help you build the website and API your product deserves.</p>
+        </blockquote>
 
           <p>
             I used Amazon Web Services (AWS) as a customer for years. But, I also experienced it from the inside. I was part of the team that built and released Amazon CodeGuru Profiler as a public AWS service in 2019.
@@ -148,7 +164,7 @@ export default function HireIndex() {
             Throughout my career, I always gravitated towards working with software release infrastructure.
             I also love the web platform, creating websites and APIs, and scaling them to millions of users.
           </p>
-          <p><strong>I want to help you deliver value to your own customers safely, reliably, continuously.</strong></p>
+          <p><strong>★ I want to help you deliver value to your own customers safely, reliably, continuously.</strong></p>
         </section>
 
         <HireMeActions />
