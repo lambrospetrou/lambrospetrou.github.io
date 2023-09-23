7c8dd024342b02adcf9f3a7d1faa3018812bfc93
Lambros Petrou <lambrospetrou@gmail.com>
2023-09-23 20:32:34 +0000

    rename from /hire to /consulting

--- src/components/layout.js
+++ src/components/layout.js
@@ -110,7 +110,7 @@ const Header = ({}) => {
             <a href="/read-watch-listen/" title="My Read-Watch-Listen list">Read-Watch-Listen</a>
             <a href="/cv/" title="Lambros Petrou Resume - CV" rel="noopener" target="_blank">CV</a>
             <a href="https://temp.minibri.com" title="Minibri Temp - Share content with expiration" rel="noopener" target="_blank">Minibri Temp</a>
-            <a className="cta-hire" href="/hire/" >Hire me</a>
+            <a className="cta-hire" href="/consulting/" >Consulting</a>
             {/* <a href="/feed/rss.xml" title="Subscribe to my RSS Feed" target="_blank" rel="noopener">RSS Feed</a> */}
           </div>
         </div>
