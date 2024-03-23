5ee8a6a2d8c1aa922352c0bb405083124688a72d
Lambros Petrou <lambrospetrou@gmail.com>
2024-03-23 16:54:07 +0000

    make the headers links without the link formatting

--- src/lib/markdown.js
+++ src/lib/markdown.js
@@ -1,10 +1,26 @@
 const {marked} = require("marked");
 
+const renderer = new marked.Renderer();
+
+// Override the heading renderer for h2 to h7
+renderer.heading = function(text, level) {
+  if (level >= 2) {
+    const slugifiedText = text
+      .toLowerCase()
+      .replace(/[^\w]+/g, '-')
+      .replace(/(^-|-$)/g, '');
+    return `<h${level} id="${slugifiedText}"><a href="#${slugifiedText}">${text}</a></h${level}>`;
+  } else {
+    return marked.Renderer.prototype.heading.apply(this, arguments);
+  }
+};
+
 const options = {
   gfm: true,
   smartLists: true,
   smartypants: true,
   xhtml: true,
+  renderer,
 };
 
 const toHtml = content => marked(content, options);
